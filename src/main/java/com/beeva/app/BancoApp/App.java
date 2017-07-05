e1216fef7b3edbbafbe926b108567b83e6335156
leafargod <xrafaelx@live.com.mx>
2017-07-05 10:07:53 -0500

    inicializacion de mi repositorio BancoApp 	new file:   src/main/java/com/beeva/app/BancoApp/App.java 	new file:   src/test/java/com/beeva/app/BancoApp/AppTest.java

---
+++ src/main/java/com/beeva/app/BancoApp/App.java
@@ -0,0 +1,13 @@
+package com.beeva.app.BancoApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
